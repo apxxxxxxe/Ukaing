d6cf79c54625085de5d5f1e1dc2990d35a01e67c
apxxxxxxe <calcium629@gmail.com>
2023-10-28 03:52:23 +0900

    init

---
+++ README.md
@@ -0,0 +1,43 @@
+[![Ukaing.nar](https://img.shields.io/github/v/release/apxxxxxxe/Ukaing?color=%235865f2&label=Ukaing.nar&logo=github)](https://github.com/apxxxxxxe/Ukaing/releases/latest/download/Ukaing.nar)
+[![commits](https://img.shields.io/github/last-commit/apxxxxxxe/Ukaing?color=%235865f2&label=%E6%9C%80%E7%B5%82%E6%9B%B4%E6%96%B0&logo=github)](https://github.com/apxxxxxxe/Ukaing/commits/main)
+
+# 伺かプラグイン「Ukaing」
+
+![ukaing](https://github.com/apxxxxxxe/Ukaing/assets/39634779/2528ce3f-89f9-40a2-8bbe-51d84cc6146f)
+
+- SSPで動作確認
+
+## 何をするもの？
+現在起動中のゴーストをDiscordのプロフィール欄に表示することができるプラグインです。
+
+## どうやって使うの？
+
+### 1. Discord側の設定
+![image](https://github.com/apxxxxxxe/Ukaing/assets/39634779/146259ef-8c71-4d24-a9e8-de8caed4cda8)
+
+上図のように、Discordの設定において、`アクティビティ設定 -> アクティビティのプライバシー -> 現在のアクティビティをステータスに表示する`が有効になっていることを確認してください。
+
+### 2. SSP側の操作
+プラグインをインストールすると、自動的にDiscordに起動中のゴースト情報が表示されるようになります。
+
+## プラグイン「Discord Rich Presence PLUGIN」との違いは？
+本プラグインは、CSaori Project様によるプラグイン「[Discord Rich Presence PLUGIN](https://ssp.shillest.net/warehouse.html)」と類似した機能を持ちますが、  以下の違いがあります。
+
+### ゴーストの配布元サイト(craftmanurl)へのボタンが表示される
+![278700554-99818f94-7d58-4aff-ab1b-975a5afa8b9a](https://github.com/apxxxxxxe/Ukaing/assets/39634779/e0f20f7c-8cb0-407d-8035-42bf2bcda266)
+![image](https://github.com/apxxxxxxe/Ukaing/assets/39634779/ac88fbae-bfd4-48ec-9786-0063775fd7b2)
+
+お気に入りゴーストの布教が容易になります。
+
+## ちょっと詳しい解説
+複数ゴーストを同時に起動していても、表示されるゴーストの情報は１体分です。その場合、最後に起動したゴースト(=最も起動した時刻が遅いゴースト)が選ばれます。
+
+## インストール方法
+ゴーストのインストールと同様に、本プラグインのnarファイルを起動中のゴーストにドラッグ＆ドロップしてください。  
+
+## 注意
+インストール直後はバージョンが古い場合があるため、必ずネットワーク更新を行ってください。  
+本プラグインの右クリックメニューからネットワーク更新が可能です。
+
+## 最新版ダウンロード
+[![Ukaing.nar](https://img.shields.io/github/v/release/apxxxxxxe/Ukaing?color=%235865f2&label=Ukaing.nar&logo=github)](https://github.com/apxxxxxxe/Ukaing/releases/latest/download/Ukaing.nar)
---
+++ src/lib.rs
@@ -0,0 +1,102 @@
+mod client;
+mod events;
+mod request;
+mod response;
+
+use client::RpcClient;
+use request::PluginRequest;
+
+use std::fs::File;
+use std::panic;
+use std::path::Path;
+
+use once_cell::sync::Lazy;
+
+use winapi::ctypes::c_long;
+use winapi::shared::minwindef::{BOOL, HGLOBAL, TRUE};
+
+use shiori_hglobal::*;
+use shiorust::message::Parser;
+
+#[macro_use]
+extern crate log;
+extern crate simplelog;
+
+use simplelog::*;
+
+static mut RPCCLIENT: Lazy<RpcClient> = Lazy::new(|| RpcClient::new());
+
+#[no_mangle]
+pub extern "cdecl" fn load(h: HGLOBAL, len: c_long) -> BOOL {
+    let v = GStr::capture(h, len as usize);
+    let s = v.to_utf8_str().unwrap();
+
+    let log_path = Path::new(s).join("ukaing.log");
+    WriteLogger::init(
+        LevelFilter::Debug,
+        Config::default(),
+        File::create(log_path).unwrap(),
+    )
+    .unwrap();
+
+    panic::set_hook(Box::new(|panic_info| {
+        debug!("{}", panic_info);
+    }));
+
+    debug!("load");
+    unsafe { RPCCLIENT.start() };
+
+    return TRUE;
+}
+
+#[no_mangle]
+pub extern "cdecl" fn unload() -> BOOL {
+    debug!("unload");
+    unsafe { RPCCLIENT.close() };
+    return TRUE;
+}
+
+#[no_mangle]
+pub extern "cdecl" fn request(h: HGLOBAL, len: *mut c_long) -> HGLOBAL {
+    // リクエストの取得
+    let v = unsafe { GStr::capture(h, *len as usize) };
+
+    let s = v.to_utf8_str().unwrap();
+
+    let pr = PluginRequest::parse(&s).unwrap();
+    let r = pr.request;
+
+    let response = events::handle_request(&r);
+
+    let bytes = response.to_string().into_bytes();
+    let response_gstr = GStr::clone_from_slice_nofree(&bytes);
+
+    unsafe { *len = response_gstr.len() as c_long };
+    response_gstr.handle()
+}
+
+#[cfg(test)]
+mod test {
+    const TOKEN: &str = "1033946714102562826";
+    use discord_rich_presence::{
+        activity::{Activity, Button, Timestamps},
+        DiscordIpc, DiscordIpcClient,
+    };
+    use std::time::Duration;
+
+    #[test]
+    fn test_client() {
+        let mut client = DiscordIpcClient::new(TOKEN).unwrap();
+        client.connect().unwrap();
+        let activity = Activity::new()
+            .state("t ")
+            .timestamps(Timestamps::new().start(chrono::Local::now().timestamp()))
+            .buttons(vec![Button::new(
+                "配布元 / craftmanurl",
+                "https://example.com/",
+            )]);
+        client.set_activity(activity).unwrap();
+        std::thread::sleep(Duration::from_secs(10));
+        client.close().unwrap();
+    }
+}
